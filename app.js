0f614b24db4c1cb7137cc1ee8069094c9de66fb0
Rafiqul_Pust <rafiqul.pust.cse@gmail.com>
2020-10-22 21:22:27 +0600

    add javascript project

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+
+const colors = ['red','yellow','green','blue','orange','purple', 'indigo'];
+
+const container = document.querySelector('section');
+const h1 = document.querySelector('h1');
+const selectedColor = document.querySelector('#selectedColor')
+for (let color of colors) {
+
+    const box = document.createElement('div');
+     box.classList.add('box');
+
+     container.appendChild(box);
+     box.style.background = color;
+     box.addEventListener('click',function(){
+          h1.innerText = 'You have selected: ';
+          selectedColor.innerText= box.style.backgroundColor;
+          selectedColor.style.backgroundColor = box.style.backgroundColor
+     });
+    
+}
